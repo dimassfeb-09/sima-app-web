3146cbfa766caa7682d608a34805b21e1768dae8
Dimas Febriyanto <dimassfeb@gmail.com>
2024-08-27 11:46:50 +0700

    notification new report

--- src/pages/HereMap.tsx
+++ src/pages/HereMap.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef } from "react";
 import { User } from "../types/user";
 
 // Extend the Window interface to include HERE Maps types
